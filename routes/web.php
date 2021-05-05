bf4dbc7050ef79cfc7352373643a80c198d78a27
Kit Corbita <kit.c@citronworks.com>
2021-05-05 19:18:07 +0800

    Add new login design and set front-end spa

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth'])->name('dashboard');
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
+
+
+Route::get('/dashboar','DashboardController@index')->name('dashboard');
 
 require __DIR__.'/auth.php';
